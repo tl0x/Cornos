c239bb49fe122afa279c43f3b03bac72d60a75c4
0x150 <constantindiscord@gmail.com>
2021-03-25 19:12:50 +0100

    hamburgers fit well with burgers - default prefix is now .

--- src/main/java/me/constantindev/ccl/module/ext/ClientConfig.java
+++ src/main/java/me/constantindev/ccl/module/ext/ClientConfig.java
@@ -6,7 +6,7 @@ import me.constantindev.ccl.etc.config.MultiOption;
 public class ClientConfig extends Module {
     public ClientConfig() {
         super("ClientConfig", "Configuration for the client.");
-        this.mconf.add(new MultiOption("prefix", "}", new String[]{"}", ".", "-", "+", "#", "@", "&", "%", "$"}));
+        this.mconf.add(new MultiOption("prefix", ".", new String[]{"}", ".", "-", "+", "#", "@", "&", "%", "$"}));
         this.mconf.add(new MultiOption("homescreen", "client", new String[]{"client", "vanilla"}));
         this.mconf.add(new MultiOption("mpscreen", "client", new String[]{"client", "vanilla"}));
     }
