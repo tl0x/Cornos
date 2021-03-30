cb6a750d5e5c78ea2f22ea0bfea8f0af5e499d83
0x150 <constantindiscord@gmail.com>
2021-03-31 00:50:26 +0200

    updated build deps, MAKE SURE TO UPDATE YOUR FABRIC LAUNCHER AND FABRIC API WHEN USING THIS BUILD BEFORE COMPLAINING!

--- src/main/java/me/constantindev/ccl/command/Test.java
+++ src/main/java/me/constantindev/ccl/command/Test.java
@@ -1,6 +1,9 @@
 package me.constantindev.ccl.command;
 
+import me.constantindev.ccl.Cornos;
 import me.constantindev.ccl.etc.base.Command;
+import net.minecraft.client.render.debug.PathfindingDebugRenderer;
+import net.minecraft.entity.mob.ZombieEntity;
 
 public class Test extends Command {
 
@@ -11,7 +14,6 @@ public class Test extends Command {
     @Override
     public void onExecute(String[] args) {
         //Cornos.minecraft.player.setPos(Double.NaN,Double.NaN,Double.NaN);
-
         super.onExecute(args);
     }
 }
