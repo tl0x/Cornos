2b3da6e86d85e91d2d9a033195f3b3e640623ddb
0x150 <constantindiscord@gmail.com>
2021-04-18 13:56:34 +0200

    made splash more gamer

--- src/main/java/me/constantindev/ccl/mixin/gui/SplashScreenMixin.java
+++ src/main/java/me/constantindev/ccl/mixin/gui/SplashScreenMixin.java
@@ -3,28 +3,25 @@ package me.constantindev.ccl.mixin.gui;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.BackgroundHelper;
 import net.minecraft.client.gui.screen.SplashScreen;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.awt.*;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-
 @Mixin(SplashScreen.class)
 public class SplashScreenMixin {
+    @Mutable
+    @Shadow @Final private static int BRAND_RGB;
+
+    @Mutable
+    @Shadow @Final private static int BRAND_ARGB;
+
     @Inject(method="init",at=@At("TAIL"))
-    private static void i(MinecraftClient client, CallbackInfo ci) throws NoSuchFieldException, IllegalAccessException {
-        Field BRAND_RGB = SplashScreen.class.getDeclaredField("BRAND_RGB");
-        Field BRAND_ARGB = SplashScreen.class.getDeclaredField("BRAND_ARGB");
-        BRAND_RGB.setAccessible(true);
-        BRAND_ARGB.setAccessible(true);
-        Field modifiersField = Field.class.getDeclaredField("modifiers");
-        modifiersField.setAccessible(true);
-        modifiersField.setInt(BRAND_RGB, BRAND_RGB.getModifiers() & ~Modifier.FINAL);
-        modifiersField.setInt(BRAND_ARGB, BRAND_ARGB.getModifiers() & ~Modifier.FINAL);
-        BRAND_RGB.set(null, BackgroundHelper.ColorMixer.getArgb(255,0,0,0) & 16777215);
-        BRAND_ARGB.set(null, BackgroundHelper.ColorMixer.getArgb(255,0,0,0));
+    private static void i(MinecraftClient client, CallbackInfo ci) {
+        BRAND_RGB = BackgroundHelper.ColorMixer.getArgb(255,0,0,0) & 16777215;
+        BRAND_ARGB = BackgroundHelper.ColorMixer.getArgb(255,0,0,0);
     }
 }
