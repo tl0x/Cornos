4a6d4b47e3b7fa4b7b43432f7a1f86c3cc3cb815
0x150 <constantindiscord@gmail.com>
2021-05-01 14:01:51 +0200

    me lon

--- src/main/java/me/constantindev/ccl/module/RENDER/Animations.java
+++ src/main/java/me/constantindev/ccl/module/RENDER/Animations.java
@@ -3,6 +3,7 @@ package me.constantindev.ccl.module.RENDER;
 import me.constantindev.ccl.Cornos;
 import me.constantindev.ccl.etc.base.Module;
 import me.constantindev.ccl.etc.config.MultiOption;
+import me.constantindev.ccl.etc.config.Num;
 import me.constantindev.ccl.etc.ms.MType;
 import me.constantindev.ccl.etc.reg.ModuleRegistry;
 import net.minecraft.client.options.Perspective;
@@ -17,11 +18,25 @@ import net.minecraft.util.math.Quaternion;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 public class Animations extends Module {
-    private static final MultiOption type = new MultiOption("type", "laidBack", new String[]{"laidBack", "small", "smaller"});
+    private static final MultiOption type = new MultiOption("type", "laidBack", new String[]{"laidBack", "small", "smaller", "custom"});
+    private static final Num customOffX = new Num("customX",0, 5, -5);
+    private static final Num customOffY = new Num("customY",0,5,-5);
+    private static final Num customOffZ = new Num("customZ",0,5,-5);
+    private static final Num customScale = new Num("customScale", 1, 5, 0);
+    private static final Num customRotX = new Num("customMX", 0, 5, -5);
+    private static final Num customRotY = new Num("customMY", 0, 5, -5);
+    private static final Num customRotZ = new Num("customMZ", 0, 5, -5);
 
     public Animations() {
         super("Animations", "makes items go brrr", MType.RENDER);
         this.mconf.add(type);
+        this.mconf.add(customOffX);
+        this.mconf.add(customOffY);
+        this.mconf.add(customOffZ);
+        this.mconf.add(customRotX);
+        this.mconf.add(customRotY);
+        this.mconf.add(customRotZ);
+        this.mconf.add(customScale);
     }
 
     public static void render(LivingEntity entity, ItemStack stack, ModelTransformation.Mode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci, ItemRenderer itemRenderer) {
@@ -38,6 +53,11 @@ public class Animations extends Module {
                 case "smaller":
                     matrices.scale(.05f, .05f, .05f);
                     matrices.translate(0, 3, 0);
+                    break;
+                case "custom":
+                    matrices.scale((float) customScale.getValue(),(float)customScale.getValue(),(float)customScale.getValue());
+                    matrices.translate(customOffX.getValue(), customOffY.getValue(), customOffZ.getValue());
+                    matrices.multiply(new Quaternion((float)customRotX.getValue(),(float)customRotY.getValue(),(float)customRotZ.getValue(),1f));
                     break;
                 default:
                     return;
