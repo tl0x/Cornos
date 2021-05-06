0b173f68def744c60d14583719ca7372d84320b0
0x150 <constantindiscord@gmail.com>
2021-05-06 19:01:29 +0200

    increased swag portion of notifications

--- src/main/java/me/constantindev/ccl/etc/NotificationManager.java
+++ src/main/java/me/constantindev/ccl/etc/NotificationManager.java
@@ -13,6 +13,8 @@ import me.constantindev.ccl.Cornos;
 import me.constantindev.ccl.module.ext.Hud;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.StringVisitable;
+import net.minecraft.text.Text;
 import net.minecraft.util.math.MathHelper;
 
 import java.awt.*;
@@ -42,34 +44,43 @@ public class NotificationManager {
         int offset = Cornos.minecraft.player.getStatusEffects().size() * 10 + 2;
         int width = Cornos.minecraft.getWindow().getScaledWidth();
         int height = Cornos.minecraft.getWindow().getScaledHeight();
+        double val = 0.0;
+        if (curr-lastCurr>3) {
+            val = 0.01;
+            lastCurr = curr;
+        }
         for (Notification notification : Lists.reverse(notifs)) {
-            if (notification.animationProgress2 < 6.0 && !notification.isAnimationComplete) {
-                notification.animationProgress2 += (((double) curr) - ((double) lastCurr)) / 100.0;
-                notification.animationProgress2 = MathHelper.clamp(notification.animationProgress2, 0, 6.1);
-                notification.animationProgress = ((Math.log(notification.animationProgress2) + 2) / 2.778);
+            if (notification.animationProgress2 < 1.0 && !notification.isAnimationComplete) {
+                notification.animationProgress2 += val;
+                notification.animationProgress2 = MathHelper.clamp(notification.animationProgress2, 0, 1.0);
+                notification.animationProgress = easeOutCubic(notification.animationProgress2);
             } else notification.isAnimationComplete = true;
-            double xOff = 150.0 * notification.animationProgress;
             if (notification.duration + notification.creationTime < curr && notification.isAnimationComplete) {
                 if (notification.animationProgress2 > 0.01) {
-                    notification.animationProgress2 -= (((double) curr) - ((double) lastCurr)) / 100.0;
-                    notification.animationProgress2 = MathHelper.clamp(notification.animationProgress2, 0, 6.1);
-                    notification.animationProgress = ((Math.log(Math.max(notification.animationProgress2, 0.01)) + 2) / 2.778);
+                    notification.animationProgress2 -= val;
+                    notification.animationProgress2 = MathHelper.clamp(notification.animationProgress2, 0, 1.0);
+                    notification.animationProgress = easeOutCubic(notification.animationProgress2);
                 } else notification.markedForDeletion = true;
-
             }
+            double xOff = 150.0 * notification.animationProgress;
             int dheight = (notification.description.length) * 9;
             dheight = dheight == 0 ? 10 : dheight + 12;
             Color rc = Hud.themeColor.getColor();
             DrawableHelper.fill(matrices, (int) (width - xOff - 1), height - offset - dheight - 1, width, height - offset + 1, rc.getRGB());
             DrawableHelper.fill(matrices, (int) (width - xOff), height - offset - dheight, width - 3, height - offset, new Color(30, 30, 30).getRGB());
-            Cornos.minecraft.textRenderer.draw(matrices, notification.title, (float) ((width - xOff + 1)), (float) ((height - offset - dheight + 1)), 0xFFFFFFFF);
+            String t = Cornos.minecraft.textRenderer.trimToWidth(notification.title,(int)Math.floor(xOff));
+            Cornos.minecraft.textRenderer.draw(matrices, t, (float) ((width - xOff + 1)), (float) ((height - offset - dheight + 1)), 0xFFFFFFFF);
             int off1 = 12;
             for (String s : notification.description) {
-                Cornos.minecraft.textRenderer.draw(matrices, s, (float) ((width - xOff + 1)), (float) ((height - offset - dheight + off1)), 0xFFFFFFFF);
+                StringVisitable td = Cornos.minecraft.textRenderer.trimToWidth(Text.of(s),(int)Math.floor(xOff));
+                Cornos.minecraft.textRenderer.draw(matrices, td.getString(), (float) ((width - xOff + 1)), (float) ((height - offset - dheight + off1)), 0xFFFFFFFF);
                 off1 += 9;
             }
             offset += dheight + 3;
         }
-        lastCurr = curr;
+    }
+
+    double easeOutCubic(double x) {
+        return 1 - Math.pow(1 - x, 3);
     }
 }
