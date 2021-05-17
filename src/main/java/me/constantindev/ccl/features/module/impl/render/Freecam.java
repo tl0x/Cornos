e5aed02e459486e5e04f48cb7d00c46b8a73eebd
0x150 <constantindiscord@gmail.com>
2021-05-17 19:46:51 +0200

    major bruh moment in #f4bf450d947bd5321647611c210bf73627b6cd5a

--- src/main/java/me/constantindev/ccl/features/module/impl/render/Freecam.java
+++ src/main/java/me/constantindev/ccl/features/module/impl/render/Freecam.java
@@ -8,32 +8,14 @@ import me.constantindev.ccl.etc.event.arg.PacketEvent;
 import me.constantindev.ccl.etc.helper.Renderer;
 import me.constantindev.ccl.features.module.Module;
 import me.constantindev.ccl.features.module.ModuleType;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.ChestBlock;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.DrawableHelper;
-import net.minecraft.client.gui.screen.ingame.GenericContainerScreen;
-import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.client.options.GameOptions;
 import net.minecraft.client.options.Perspective;
-import net.minecraft.client.render.OverlayTexture;
-import net.minecraft.client.render.model.BakedModel;
-import net.minecraft.client.render.model.json.ModelTransformation;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.EntityPose;
-import net.minecraft.inventory.Inventory;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerInputC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerInteractBlockC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
-import net.minecraft.screen.GenericContainerScreenHandler;
-import net.minecraft.screen.ScreenHandlerFactory;
-import net.minecraft.text.Text;
-import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 
 import java.awt.*;
@@ -59,10 +41,10 @@ public class Freecam extends Module {
             if (pe.packet instanceof PlayerMoveC2SPacket) {
                 event.cancel();
             }
-            if(pe.packet instanceof PlayerInputC2SPacket) {
+            if (pe.packet instanceof PlayerInputC2SPacket) {
                 event.cancel();
             }
-            if(pe.packet instanceof PlayerInteractBlockC2SPacket) {
+            if (pe.packet instanceof PlayerInteractBlockC2SPacket) {
                 event.cancel();
             }
             if (pe.packet instanceof PlayerActionC2SPacket) {
